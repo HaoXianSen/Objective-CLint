9a6ca9dd3daf846a08520b7600b76d44db56b452
郝玉鸿 <haoyh02@rd.netease.com>
2023-10-26 11:07:18 +0800

    调试问题

--- Demo/Demo/AppDelegate.mm
+++ Demo/Demo/AppDelegate.mm
@@ -15,7 +15,8 @@
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication *)application
+ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     //    @[
     //        @{
